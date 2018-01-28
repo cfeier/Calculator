cc9f713d556c7394324c398d10141b889d6cbd6d
cfeier <feierabend.colin@gmail.com>
2018-01-28 14:42:17 +0100

    changed travis.yml configuration

--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -2,20 +2,24 @@ import static org.junit.Assert.assertEquals;
 
 import org.junit.Test;
 
+/**
+ * 
+ * @author Colin Feierabend
+ */
 public class CalculatorTest {
 
     @Test
-    public void twoPositiveNumbersTest() { 
+    public void addTwoPositiveNumbersTest() { 
         assertEquals(5, Calculator.add(2, 3));
     }
     
     @Test
-    public void twoNegativeNumbersTest() {
+    public void addTwoNegativeNumbersTest() {
         assertEquals(-5, Calculator.add(-2, -3));
     }
     
     @Test
-    public void twoZerosTest() {
+    public void addTwoZerosTest() {
         assertEquals(0, Calculator.add(0, 0));
     }
 }
